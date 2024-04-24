5c21992a1cf076fd5211309efde914e3d5d2eb85
Ferry Setiawan <ferryyoyo@gmail.com>
2024-04-24 15:06:30 +0700

    Initial Commit

---
+++ LottieTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieTutorial
+//
+//  Created by Ferry Setiawan on 24/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
